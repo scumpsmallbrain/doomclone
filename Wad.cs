d5af2f814b5249472edcf9ccd564b46be4673421
Scump Smallbrain <scumpsmallbrain@gmail.com>
2024-09-14 23:46:39 -0400

    start wad reading code. and i mean really just barely start

--- Wad.cs
+++ Wad.cs
@@ -2,5 +2,23 @@ namespace ScumpDoom;
 
 public partial class SD
 {
-    
+    static void WADInit()
+    {
+
+    }
+}
+
+
+public class WADReader
+{
+    readonly FileStream? wadFile = null;
+
+    public WADReader(string wad_path)
+    {
+        wadFile = File.Open(wad_path, FileMode.Open, FileAccess.Read);
+        if ( wadFile == null )
+            Environment.Exit(66);
+        
+    }
+
 }
